d1f17c5ada6ab66d333357600c39694dd85c24df
Akash Lande <akashlande97@gmail.com>
2023-08-28 23:31:01 +0530

    episode6

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,43 +1,76 @@
-import { useState ,useEffect} from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 
-
-
 const Body = () => {
-  const [resList,setReslist]=useState([]);
+  const [resList, setReslist] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestaurant,setFilteredRestaurants]=useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
 
-  const fetchData =async()=>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.83641491733662&lng=75.2922973036766&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-    const json =await data.json();
-    console.log(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
-    setReslist(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-  }
-
-  if (resList.length === 0) { // Check if resList is empty
-    return <Shimmer />;
-  }
-    return (
-      <div className="body">
-        <div className="filter">
-          <button className="filter-btn" onClick={()=>{
-            const filteredList = resList.filter(
-              (res) =>res.info.avgRating >4
-            );
-            setReslist(filteredList)
-          }}>Top Rating Restro</button>
-        </div>
-        <div className="res-container">
-          {resList.map((restaurant) => (
-            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-          ))}
-        </div>
-      </div>
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.83641491733662&lng=75.2922973036766&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+    const json = await data.json();
+    console.log(
+      json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setReslist(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurants( json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   };
 
-  export default Body;
+  if (resList.length === 0) {
+    // Check if resList is empty
+    return <Shimmer />;
+  }
+  return (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-text"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filterrestaurant = resList.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurants(filterrestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = resList.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setReslist(filteredList);
+          }}
+        >
+          Top Rating Restro
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
