facd9e8afe8677c95ae7129c13367b2c95359f62
Akash Lande <akashlande97@gmail.com>
2023-09-08 19:24:21 +0530

    episode 11

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -1,26 +1,20 @@
-import {  ITEM_URL } from "../utils/constants";
+import { ITEM_URL } from "../utils/constants";
 
-const ItemList =({items})=>{
-    console.log("item Data is",items);
-    return(
+const ItemList = ({ items }) => {
+  console.log("item Data is", items);
+  return (
     <div>
-        {items.map((item)=>(
-            <div key={item.card.info.id}>
-                <div className="p-2 m-2 border-gray-200 border-b-2">
-                    <div className="py-2">
-                    <img scr ={ITEM_URL + item.card.info.imageId}/>
-                    <span>{item.card.info.name}{"-   ₹ "}</span>
-                    <span>{item.card.info.price /100}</span>
-                    <p className="text-xs">{item.card.info.description}</p>
-                    </div>
-                    
-                    
-                </div>
-                
-            </div>
-        ))}
+      {items.map((item) => (
+        <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 rounded-lg">
+          <div className="py-2">
+            <img className="mx-auto" src={ITEM_URL + item.card.info.imageId} alt={item.card.info.name} />
+            <span className="block text-center">{item.card.info.name} - ₹ {(item.card.info.price / 100).toFixed(2)}</span>
+            <p className="text-xs text-center">{item.card.info.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
-    )
-}
+  );
+};
 
 export default ItemList;
