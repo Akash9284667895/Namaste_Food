1d31ff347f8d5424ece6f062436ae6b317602c33
Akash Lande <akashlande97@gmail.com>
2023-09-09 14:49:16 +0530

    episode 11

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -2,7 +2,7 @@ import { ITEM_URL } from "../utils/constants";
 
 const ItemList = ({ items }) => {
   // console.log("item Data is", items);
-  
+
   return (
     <div>
       {items.map((item) => (
@@ -16,12 +16,15 @@ const ItemList = ({ items }) => {
               src={ITEM_URL + item.card.info.imageId}
               alt={item.card.info.name}
             />
-            <button
-              className="absolute top-2 right-2 bg-blue-500 text-white px-2 my-5 py-1 rounded-full hover:bg-blue-600 hover:text-gray-100"
-            >
+            <button className="absolute top-2 right-2 bg-blue-500 text-white px-2 my-5 py-1 rounded-full hover:bg-blue-600 hover:text-gray-100">
               Add +
             </button>
-            <span className="block text-center">{item.card.info.name} - ₹ {(item.card.info.price / 100).toFixed(2)}</span>
+            <span className="block text-center">
+              {item.card.info.name} - ₹{" "}
+              {(
+                (item.card.info.price || item.card.info.defaultPrice) / 100
+              ).toFixed(2)}
+            </span>
             <p className="text-xs text-center">{item.card.info.description}</p>
           </div>
         </div>
